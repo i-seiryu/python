eff6a3ea81e99087ea3aeb638a612aad4fcf80dc
i-seiryu <33116680+i-seiryu@users.noreply.github.com>
2017-10-27 23:55:19 +0900

    Variables and data types

--- investment.py
+++ investment.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-amount = float(input("Enter amount: "))  # 输入数额
-inrate = float(input("Enter Interest rate: "))  # 输入利率
-period = int(input("Enter period: "))  # 输入期限
+amount = float(input("Enter amount: ")) 
+# 输入数额
+inrate = float(input("Enter Interest rate: "))  
+# 输入利率
+period = int(input("Enter period: ")) 
+# 输入期限
 value = 0
 year = 1
 while year <= period:
